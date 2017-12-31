fe44a27ec0d62f2a2d30fd8399cea94fd96a4dc9
Keerthana <34229113+keer707@users.noreply.github.com>
2017-12-31 19:36:29 +0530

    Update Headers.js

--- Headers.js
+++ Headers.js
@@ -1,30 +1,53 @@
 import React,{Component} from 'react';
-import {  Header, Left, Body, Button,Thumbnail, Text } from 'native-base';
-import {TouchableWithoutFeedback} from 'react-native';
+import {  Header,Content, Left, Body, Button,Thumbnail,Title,Item,Input,Icon,Right, Text } from 'native-base';
+import {TouchableWithoutFeedback,StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
-
+import SearchScreen from './SearchScreen';
+import SearchTab from './SearchTab';
 const Headers = (props) => {
- 
-const {navigation}=props;  
+const {navigation,tabKey}=props;  
+console.log(navigation);
+let body=null;
+let right=null;
+if(tabKey=='Search')
+{right=<Right style={{marginRight:10,marginTop:20}}>
+  <Icon style={styles.iconStyle} name='ios-person-add-outline'/></Right>}
+if(tabKey=='Search')
+{body=<Body style={{marginRight:0,marginLeft:80}}>
+  <Button rounded style={{width: 250,height:40, backgroundColor: 'lightgrey',marginTop:20}} 
+  onPress={() =>navigation.navigate('SearchTab')}>
+    <Title style={{color:'grey'}}> Search Twitter </Title>
+    </Button> 
+   </Body>;
+   
+  } else {
+    body=<Body style={{marginRight:200}}>
+    <Title style={{color: 'black',fontSize: 20,marginTop:20, fontWeight: 'bold'}}>{tabKey}</Title>
+    </Body>
+  }
     return (
  
-        <Header hasTabs style={{backgroundColor:'#fff',height:100}}>
+        <Header hasTabs='true' style={{backgroundColor:'#fff',height:80,elevation:0 }}>
           <Left>
            <Button transparent onPress={()=>navigation.navigate("DrawerOpen")}
-            style={{backgroundColor:'#fff',height:60}}>
-           <Thumbnail source={require('./pic1.jpg')}  />
+            style={{backgroundColor:'white',height:40,marginTop:20}}>
+           <Thumbnail source={require('./pic1.jpg')} small />
             </Button>
           </Left>
-          <Body>
-            <Text style={{fontSize: 24, fontWeight: 'bold'}}>Home</Text>
-          </Body>
-
+          {body}
+          {right}
         </Header>
 
     );
+};
+const styles =StyleSheet.create({
+  
+  iconStyle:{
+      color:'#007aff',
+      
+  }
+ 
 
-    
-
-}
+});
 
 export default Headers ;
