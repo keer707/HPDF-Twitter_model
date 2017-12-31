eb03b3a9d28dedbbde9bb330f0e4ffd1eed4e834
Keerthana <34229113+keer707@users.noreply.github.com>
2017-12-31 19:30:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,40 +1,59 @@
-import React, { Component } from "react";
-import HomeScreen from './HomeScreen';
-import SearchScreen from './SearchScreen';
+import React,{Component} from 'react';
+import {TabNavigator,StackNavigator,DrawerNavigator} from 'react-navigation';
 import Notifications from './Notifications';
 import Message from './Message';
-import { TabNavigator, StackNavigator, DrawerNavigator } from "react-navigation";
-import Sidebar from './Sidebar';
+import HomeScreen from './HomeScreen';
+import  Footers  from './Footers';
+import Headers from './Headers.js';
+import SearchScreen from './SearchScreen';
+import SearchTab from './SearchTab';
+import TabComponent from './TabComponent';
+import SideBar from './SideBar';
+import {Font} from 'expo';
 
-import { ScrollView, Text } from 'react-native';
+var MainScreenNavigator= TabNavigator({
+  Home:{screen: HomeScreen},
+  Search:{screen:SearchScreen},
+  Notifications: {screen:Notifications},
+  Message:{screen: Message}
 
-
-const TabList = TabNavigator({
-  Home: {screen: HomeScreen},
-  Search: { screen: SearchScreen},
-  Notifications: {screen: Notifications}, 
-  Message: {screen: Message},
-	},
-		
- 
-  
- );
-
-
-
-const HomeScreenRouter = DrawerNavigator(
-  {
-    Home: { screen: TabList  }
-    
+},{ 
+  tabBarComponent:props=>(<TabComponent {...props}/>),
+  tabBarPosition:'top',
+  animationEnabled:true,
+  tabBarOptions:{
+    activeTintColor:'#007aff',
+    inactiveTintColor:'grey',
+    activeBackgroundColor:'blue',
+    inactiveBackgroundColor:'#fff',
+    showIcon:true,
+    showLabel:false,
+    tabBgColor:"#fff",
+    topTabActiveBgColor: "#fff",
+    topTabBarActiveBorderColor: "#007aff"
   },
-  {
-    contentComponent: props => <Sidebar {...props} />
-  }
+  
+});
+
+
+const Router = DrawerNavigator({
+  Home: { screen: MainScreenNavigator,
+    navigationOptions: {    }, },
+  
+ },
+{contentComponent:props=>(<SideBar {...props}/>),
+drawerWidth:350}
 );
 
 const MyAppNavigator = StackNavigator({
-  Home: { screen: TabList },
-  
- });
+  Home: { screen: Router,
+    navigationOptions:{header:null},
+  },
+  SearchTab:{screen:SearchTab,
+  navigationOptions:{header:null},} 
+});
 
-export default TabList;
+
+ 
+
+export default MyAppNavigator;
